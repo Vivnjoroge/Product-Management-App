a3c935d8bf6e1b72850dadfa121269b7f863ca26
Vivian Njoroge <viviannjoroge91@gmail.com>
2025-11-25 13:18:26 +0300

    fix errors

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
 const Login = () => import('../pages/Login.vue')
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
