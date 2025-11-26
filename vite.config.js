28d129d1abe650470c9db63a3cbfff476231707a
Vivian Njoroge <viviannjoroge91@gmail.com>
2025-11-26 06:19:03 +0300

    correct base and production path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Product-Management-App/'
-
+  base: process.env.NODE_ENV === 'production' ? '/Product-Management-App/' : '/',
 })
